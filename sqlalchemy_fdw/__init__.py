64234ac9246a7f1777cc06b2c4cdae4101ba5357
Cédric <cedric.migazzi@kozea.fr>
2021-06-24 10:35:46 +0200

    Remove apparently useless statement to support sqlachemy 1.4

--- sqlalchemy_fdw/__init__.py
+++ sqlalchemy_fdw/__init__.py
@@ -2,14 +2,11 @@
 """
 
 from sqlalchemy.ext.compiler import compiles
-from sqlalchemy import util as sqlautil
 from sqlalchemy.schema import DDLElement, Table
 from sqlalchemy.sql.base import _bind_or_error
 from sqlalchemy import sql
 from sqlalchemy import types
 from .util import sql_options
-
-ddl = sqlautil.dependencies._importlater('sqlalchemy.engine', 'ddl')
 
 
 class ForeignTable(Table):
